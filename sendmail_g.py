76f1b69a7a7a0bd9bea1523edda145f0e30d8f7f
ChahatTandon <chahat.7874@gmail.com>
2021-05-11 23:30:35 +0530

    Add files via upload

---
+++ sendmail_g.py
@@ -0,0 +1,37 @@
+
+import smtplib
+from smtplib import SMTP
+
+
+from_mail = email
+#
+def sendmail(subject, message, to_mail):    
+	s = smtplib.SMTP('smtp.gmail.com:527')
+	s.starttls()
+	s.login(email, password)
+	message = f'Subject : {subject} \n\n {message}'
+	s.sendmail(from_mail, to_mail, msg.as_string())
+	s.sendmail(from_mail, to_mail, message)
+	s.quit()
+
+
+
+
+# import smtplib
+
+# sender = 'chahat.7874@gmail.com'
+# receivers = ['to@todomain.com']
+
+# message = """From: From Person <chahat.7874@gmail.com>
+# To: To Person <to@todomain.com>
+# Subject: SMTP e-mail test
+
+# This is a test e-mail message.
+# """
+
+# try:
+#    smtpObj = smtplib.SMTP('localhost')
+#    smtpObj.sendmail(sender, receivers, message)         
+#    print "Successfully sent email"
+# except SMTPException:
+#    print "Error: unable to send email"
